529394f8020d34ff4cdd362e26fc6800c76b7955
jtayped <jtayped@gmail.com>
2024-02-21 23:30:33 +0100

    back button aria label

--- src/app/track/[id]/page.jsx
+++ src/app/track/[id]/page.jsx
@@ -29,6 +29,7 @@ const TrackPage = async ({ params }) => {
         <div className="flex items-center w-full gap-4">
           <Link
             href="/"
+            aria-label="Back button"
             className="text-white/50 hover:text-white/90 hover:bg-white/10 transition-colors bg-white/5 backdrop-blur-md border border-white/10 rounded-xl p-4"
           >
             <FaArrowLeft />
