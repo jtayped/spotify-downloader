3a7ec623095a5d4a10e41d4125c7a66674dc48e1
Joel Taylor <jtayped@gmail.com>
2024-02-27 19:45:48 +0100

    downloading animations

--- src/components/buttons/DownloadPlaylist.jsx
+++ src/components/buttons/DownloadPlaylist.jsx
@@ -3,6 +3,7 @@ import { downloadBlob, getFilenameFromHeaders } from "@/lib/util";
 import axios from "axios";
 import { useState } from "react";
 import { FiDownload } from "react-icons/fi";
+import Spinner from "../Spinner";
 
 const DownloadPlaylist = ({ playlist }) => {
   const [downloading, setDownloading] = useState(false);
@@ -20,22 +21,24 @@ const DownloadPlaylist = ({ playlist }) => {
     setDownloading(false);
   };
 
-  return (
-    <button
-      onClick={() => handleDownload(playlist.id, playlist.type)}
-      aria-label="Download Playlist"
-      className="text-white flex items-center gap-3 bg-accent hover:bg-accent/90 transition-colors rounded px-5 py-2"
-    >
-      {downloading ? (
-        <span className="w-[90px]"></span>
-      ) : (
-        <>
-          <FiDownload className="text-md" />
-          Download
-        </>
-      )}
-    </button>
-  );
+  if (downloading)
+    return (
+      <div className="flex items-center gap-3 bg-accent/60 px-5 py-2 rounded">
+        <Spinner />
+        Downloading...
+      </div>
+    );
+  else
+    return (
+      <button
+        onClick={() => handleDownload(playlist.id, playlist.type)}
+        aria-label="Download Playlist"
+        className="text-white flex items-center gap-3 bg-accent hover:bg-accent/90 transition-colors rounded px-5 py-2"
+      >
+        <FiDownload className="text-md" />
+        Download
+      </button>
+    );
 };
 
 export default DownloadPlaylist;
