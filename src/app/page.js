20fac02760a715d1d35878f1b2d2eada71a6f544
Joel Taylor <jtayped@gmail.com>
2024-03-02 23:16:14 +0100

    github button

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { IoLogoGithub } from "react-icons/io";
 import Search from "@/components/Search";
 import Link from "next/link";
 
@@ -6,9 +7,9 @@ const Home = async () => {
     <main className="w-full h-full flex items-center justify-center">
       <div className="px-5 w-full md:w-[700px] space-y-5">
         <div className="text-text md:w-4/5">
-          <p className="text-xs tracking-widest">
+          <span className="text-xs tracking-widest">
             by <Link href="https://joeltaylor.business">Joel Taylor</Link>
-          </p>
+          </span>
           <h1 className="text-4xl font-bold">Spotify Downloader</h1>
           <p className="mt-2">
             Easily download spotify <b>playlists or tracks</b> by simply pasting
@@ -18,6 +19,13 @@ const Home = async () => {
             </Link>
             .
           </p>
+          <Link
+            href="https://github.com/jtayped/spotify-downloader"
+            className="flex items-center gap-2 px-5 py-2 border w-fit rounded-lg mt-2 hover:bg-white/5 transition-colors"
+          >
+            <IoLogoGithub />
+            Repository
+          </Link>
         </div>
         <Search />
       </div>
