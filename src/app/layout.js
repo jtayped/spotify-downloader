fa713db4e4a9a2037eb45074a86e8d26e1a753f5
Joel Taylor <jtayped@gmail.com>
2024-03-05 21:03:55 +0100

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import Providers from "./providers";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          defer
+          src="https://analytics.eu.umami.is/script.js"
+          data-website-id="4fdaf949-05a3-4ab3-bab2-a7faa5ac6884"
+        />
         <div className="w-full h-full fixed -z-10">
           <div className="absolute bottom-0 md:bottom-1/4 left-0 md:left-1/3 w-44 h-44 bg-accent rounded-full blur-[180px]" />
           <div className="absolute bottom-2/3 left-[100px] w-24 h-24 bg-accent rounded-full blur-[150px] hidden md:block" />
