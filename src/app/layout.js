9fec810734fedaa572e56c23244a740f90aaf69c
jtayped <jtayped@gmail.com>
2024-03-06 18:40:33 +0100

    logo/ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,11 @@ export default function RootLayout({ children }) {
           src="https://analytics.eu.umami.is/script.js"
           data-website-id="4fdaf949-05a3-4ab3-bab2-a7faa5ac6884"
         />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8031406906622541"
+          crossOrigin="anonymous"
+        />
         <div className="w-full h-full fixed -z-10">
           <div className="absolute bottom-0 md:bottom-1/4 left-0 md:left-1/3 w-44 h-44 bg-accent rounded-full blur-[180px]" />
           <div className="absolute bottom-2/3 left-[100px] w-24 h-24 bg-accent rounded-full blur-[150px] hidden md:block" />
