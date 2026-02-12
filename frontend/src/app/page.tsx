497863f97a4b290a0d5e0dcdad45c9380eed490e
jtayped <jtayped@gmail.com>
2026-02-12 16:45:53 +0100

    reset home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,7 @@
-"use server";
-import { api } from "@/lib/api";
 import React from "react";
 
-const HomePage = async () => {
-  const res = await api.get("/api/playlist/06fAr3dCUFsMf5YBsvjaq8");
-
-  return <div>{JSON.stringify(res.data)}</div>;
+const HomePage = () => {
+  return <div></div>;
 };
 
 export default HomePage;
