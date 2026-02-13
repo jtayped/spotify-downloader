365ed04dab76dc5a209e2b3fa44dbe6be148578f
jtayped <jtayped@gmail.com>
2026-02-13 20:31:58 +0100

    added error handling

--- frontend/src/app/track/[id]/page.tsx
+++ frontend/src/app/track/[id]/page.tsx
@@ -1,9 +1,13 @@
 import { api } from "@/lib/api";
+import { catchError } from "@/lib/error-handling";
+import { notFound } from "next/navigation";
 import React from "react";
 
 const TrackPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
-  const res = await api.get(`/api/track/${id}`);
+
+  const [error, res] = await catchError(api.get(`/api/track/${id}`));
+  if (error) return notFound();
 
   return <div>{JSON.stringify(res.data)}</div>;
 };
